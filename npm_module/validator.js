fea87d46d38eaa2398c0dc797041a281ef4bf4fb
Abhijeet Kumar <aryaabhi84@gmail.com>
2023-08-26 14:52:08 +0530

    changes

---
+++ npm_module/validator.js
@@ -0,0 +1,15 @@
+/*
+    if we want to create package then first need to run 
+     node init :- It will create a package.json which will carry all information about module.
+
+    now we are going to work with validator for validate.
+
+    Validator.
+
+    command: npm i validator
+*/
+
+const chalk     = require("chalk");
+const validator =  require("validator");
+const myEmail   = validator.isEmail("abc@gmail");
+console.log(myEmail ? chalk.green.inverse(myEmail) : chalk.red.inverse(myEmail));
